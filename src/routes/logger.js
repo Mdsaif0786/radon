ce55af5d55a1585c208884f949e0875111a7650d
Mdsaif0786 <mdsaifrza11@gmail.com>
2022-06-01 22:10:29 +0530

    Raj1

--- src/routes/logger.js
+++ src/routes/logger.js
@@ -1,10 +1,12 @@
 const endpoint = "https://www.functionup.org"
 const batch = "radon"
 
-const log = function() {
-    console.log('I am inside the log function')
+    const welcome =function() {
+       
+   
+    console.log('Welcome to my application. I am MD SAIF RAZA and a part of FunctionUP Radon cohort')
 }
 
 module.exports.endpoint = endpoint
 module.exports.batch = batch
-module.exports.log = log
+module.exports.log = welcome
--- src/routes/route.js
+++ src/routes/route.js
@@ -10,21 +10,6 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
-router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
+router.get('/test-me1', function(req, res) {
+    res.send('Welcome to myapplication.I am MD SAIF RAZA and a part of FunctionUp Radon cohort!')
 });
-
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
-
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
-
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
-
-module.exports = router;
-// adding this comment for no reason
